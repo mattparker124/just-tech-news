7e7a755bb5cb3ae96b1c295a71b8be8e214b47ca
Matthew Parker <mattparker124@gmail.com>
2022-05-07 01:57:51 -0500

    added hashing for update request passwords

--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -50,7 +50,9 @@ router.post('/', (req, res) => {
 
 // PUT /api/users/1
 router.put('/:id', (req, res) => {
+    // expects {username: 'Lernantino', email: 'lernantino@gmail.com', password: 'password1234'}
     User.update(req.body, {
+        individualHooks: true,
         where: {
             id: req.params.id
         }
